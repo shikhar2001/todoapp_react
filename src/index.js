a31524fdd534abae0408f55624d67edee9617bc3
shikhar2001 <imt_2019095@iiitm.ac.in>
2020-06-08 02:37:51 +0530

    Initial

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
+import './font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
   <React.StrictMode>
