13884cec3de845ff636135656f51f0d2e77e9ab4
shikhar2001 <imt_2019095@iiitm.ac.in>
2020-06-08 02:34:40 +0530

    Updates

---
+++ precache-manifest.4ec4cdb876ab085d1073c200d060b219.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b0430d4321da0dfcc0d48cd69d6b8b",
+    "url": "/todoapp_react/index.html"
+  },
+  {
+    "revision": "4454f38e137275cad2d8",
+    "url": "/todoapp_react/static/css/main.59b79525.chunk.css"
+  },
+  {
+    "revision": "1c92fcd343dead3fe4be",
+    "url": "/todoapp_react/static/js/2.9fc5f111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp_react/static/js/2.9fc5f111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4454f38e137275cad2d8",
+    "url": "/todoapp_react/static/js/main.f11e3adf.chunk.js"
+  },
+  {
+    "revision": "d65f0f52da723ec30490",
+    "url": "/todoapp_react/static/js/runtime-main.6a46cff8.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todoapp_react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todoapp_react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todoapp_react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todoapp_react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todoapp_react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
